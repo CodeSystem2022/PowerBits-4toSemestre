33ba6e260c3527bada6ddedc9726eccdd71d8370
IviiGonzalez <112676298+IviiGonzalez@users.noreply.github.com>
2023-08-24 22:39:44 -0300

    Clase 01- Video 1.6 (Parte 1)
    Comenzamos a generar las diferentes opciones en menu_app_usuario

--- Python/Leccion01/capa_datos_persona/Usuario.py
+++ Python/Leccion01/capa_datos_persona/Usuario.py
@@ -1,5 +1,5 @@
-class Usuario
-    def __init__(self, id_usuario=None, usarname=None, password=None):
+class Usuario:
+    def __init__(self, id_usuario=None, username=None, password=None):
         self._id_usuario = id_usuario
         self._username = username
         self._password = password
