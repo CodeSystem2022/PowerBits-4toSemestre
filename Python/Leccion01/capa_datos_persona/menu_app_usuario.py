4d2865e5c6d5aebb4d3ddd5da662282fd96332fc
Lucas Mendez <mendezlucas777@gmail.com>
2023-08-28 11:09:22 -0300

    Clase 1 - Video 1.6 - Comenzamos con menuAppUsuario (parte 3)

--- Python/Leccion01/capa_datos_persona/menu_app_usuario.py
+++ Python/Leccion01/capa_datos_persona/menu_app_usuario.py
@@ -1,4 +1,4 @@
-from capa_datos_persona.Usuario import Usuario
+from Usuario import Usuario
 from usuario_dao import UsuarioDAO
 from logger_base import log
 
@@ -22,7 +22,18 @@ while opcion != 5:
         usuario = Usuario(username=username_var, password=password_var)
         usuario_insertado = UsuarioDAO.insertar(usuario)
         log.info(f'Usuario insertado: {usuario_insertado}')
-
+    elif opcion == 3:
+        id_usuario_var = int(input('Digite el id del usuario a modificar: '))
+        username_var = input('Digite el nombre del usuario a modificar: ')
+        password_var = input('Digite la contraseña del usuario a modificar: ')
+        usuario = Usuario(id_usuario=id_usuario_var, username=username_var, password=password_var)
+        usuario_actualizado = UsuarioDAO.actualizar(usuario)
+        log.info(f'Usuario actualizado: {usuario_actualizado}')
+    elif opcion == 4:
+        id_usuario_var = int(input('Digite el id del usuario a eliminar: '))
+        usuario = Usuario(id_usuario=id_usuario_var)
+        usuario_eliminado = UsuarioDAO.eliminar(usuario)
+        log.info(f'Usuario eliminado: {usuario_eliminado}')
 else:
     log.info('Salimos de la aplicacion, hasta pronto!!')
 
