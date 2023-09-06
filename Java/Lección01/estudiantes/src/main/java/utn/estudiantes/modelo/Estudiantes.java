1c0512d45cfd47ad9cea9a0ab8a09bfcaf7876d4
Lucas Mendez <mendezlucas777@gmail.com>
2023-09-06 10:26:15 -0300

    Clase 3 - Video 3.2 - Parte 2 - Java
    Teoría -> Creación del paquete modelo y la clase entidad Estudiante

--- Java/Lección01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiantes.java
+++ Java/Lección01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiantes.java
@@ -1,4 +1,13 @@
 package utn.estudiantes.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Estudiantes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+    
 }
