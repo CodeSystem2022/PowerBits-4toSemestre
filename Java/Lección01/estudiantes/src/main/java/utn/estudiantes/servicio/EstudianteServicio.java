7780708a980447ab074d1cd96ae38ef1ede2571f
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-09-13 15:49:46 -0300

    Clase 4- Video 4.1 Parte 3- Metodo guardarEstudiante y eliminarEstudiante

--- Java/Lección01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Java/Lección01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -26,11 +26,11 @@ public class EstudianteServicio implements IEstudianteServicio{
 
     @Override
     public void guardarEstudiante(Estudiante estudiante) {
-
+        estudianteRepositorio.save(estudiante);
     }
 
     @Override
     public void eliminarEstudiante(Estudiante estudiante) {
-
+        estudianteRepositorio.delete(estudiante);
     }
 }
