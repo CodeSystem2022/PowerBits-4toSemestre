d62ea8ffb925e230527251c877d5fd8403418ae3
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-09-14 17:26:20 -0300

    Clase 4- Video 4.2 Parte 2- Creamos el objeto Logger
    Agregamos archivo para proyecto Spring

--- Java/Lección01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Lección01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,11 +1,19 @@
 package utn.estudiantes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
 
 @SpringBootApplication
 public class EstudiantesApplication implements CommandLineRunner {
+    @Autowired
+    private EstudianteServicio estudianteServicio;
+    private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+    
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
