567e546bef33262deccfcfc904fdacb2be19da08
Ivana <ayeivanagon@gmail.com>
2023-09-16 13:36:35 -0300

    Clase 3 - Video 3.4
    Botones de suma y resta de productos

--- JavaScript/E-commerce/client/js/cart.js
+++ JavaScript/E-commerce/client/js/cart.js
@@ -48,6 +48,19 @@ const displayCart = () => {
         </div>
         `;
         modalContainer.append(modalBody);
+
+        const decrese = modalBody.querySelector(".quantity-btn-decrese");
+        decrese.addEventListener("click", () =>{
+            if (product.quanty !== 1)
+                product.quanty--;
+                displayCart();
+        })
+
+        const increse = modalBody.querySelector(".quantity-btn-increse");
+        increse.addEventListener("click", () =>{
+            product.quanty++;
+            displayCart();
+        })
     });
 
     //Modal footer
