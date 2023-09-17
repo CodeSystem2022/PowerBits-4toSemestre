b8a35bbc9e10b03ac4e81856bd4560fd77e904a3
Facundo Rios <112650077+jfacundorios@users.noreply.github.com>
2023-09-17 11:40:39 -0300

    Clase 4 - 4.2 Parte 4

--- Java/Lección01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Lección01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -25,6 +25,6 @@ public class EstudiantesApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        logger.info("Ejecutando el método run de Spring...");
+        logger.info(nl+"Ejecutando el método run de Spring..."+nl);
     }
 }
