2fe3c71eaadf1dd1b08856b634fc0e0ac9e32f4b
Ivana <ayeivanagon@gmail.com>
2023-09-20 15:35:37 -0300

    Clase 5 - Video 5.4
    Parte 3 -> modificarEstudiante

--- Java/Lección01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Lección01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -61,6 +61,64 @@ public class EstudiantesApplication implements CommandLineRunner {
                 List<Estudiantes2022> estudiantes = estudianteServicio.listarEstudiantes();
                 estudiantes.forEach((estudiante -> logger.info(estudiante.toString()+nl)));
             }
+            case 2 -> {  //Buscar estudiante por Id
+                logger.info("Digite el id del estudiante a buscar: ");
+                var idEstudiante = Integer.parseInt(consola.nextLine());
+                Estudiantes2022 estudiante =
+                        estudianteServicio.buscarEstudiantePorId(idEstudiante);
+                if(estudiante != null){
+                    logger.info("Estudiante encontrado: "+estudiante+nl);
+                }
+                else{
+                    logger.info("Estudiante NO encontrado: "+estudiante+nl);
+                }
+            }
+            case 3 -> {  //Agregar estudiante
+                logger.info("Agregar estudiante: "+nl);
+                logger.info("Nombre: "+nl);
+                var nombre = consola.nextLine();
+                logger.info("Apellido: "+nl);
+                var apellido = consola.nextLine();
+                logger.info("Telefono: "+nl);
+                var telefono = consola.nextLine();
+                logger.info("Email: "+nl);
+                var email = consola.nextLine();
+                //Crear el objeto estudiangte sin el id
+                var estudiante = new Estudiantes2022();
+                estudiante.setNombre(nombre);
+                estudiante.setApellido(apellido);
+                estudiante.setTelefono(telefono);
+                estudiante.setEmail(email);
+                estudianteServicio.guardarEstudiante(estudiante);
+                logger.info("Estudiante agregado: "+estudiante+nl);
+            }
+            case 4 -> {   //Modificar estudiante
+                logger.info("Modificar estudiante: "+nl);
+                logger.info("Ingrese el id del estudiante: ");
+                var idEstudiante = Integer.parseInt(consola.nextLine());
+                //Buscamos el estudiante a modificar
+                Estudiantes2022 estudiante =
+                        estudianteServicio.buscarEstudiantePorId(idEstudiante);
+                if(estudiante != null){
+                    logger.info("Nombre: "+nl);
+                    var nombre = consola.nextLine();
+                    logger.info("Apellido: "+nl);
+                    var apellido = consola.nextLine();
+                    logger.info("Telefono: "+nl);
+                    var telefono = consola.nextLine();
+                    logger.info("Email: "+nl);
+                    var email = consola.nextLine();
+                    estudiante.setNombre(nombre);
+                    estudiante.setApellido(apellido);
+                    estudiante.setTelefono(telefono);
+                    estudiante.setEmail(email);
+                    estudianteServicio.guardarEstudiante(estudiante);
+                    logger.info("Estudiante modificado: "+estudiante+nl);
+                }
+                else{
+                    logger.info("Estudiante NO encontrado con el id: "+idEstudiante+nl);
+                }
+            }
         }//Fin del switch
         return salir;
     }
