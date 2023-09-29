3c3f67946cbf6b956becd724200d12b7eced8116
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2023-09-29 00:14:40 -0300

    Clase 5 - Video 5.4 parte 2
    Método join para colecciones a cadenas

--- Python/Clase01/metodo_join.py
+++ Python/Clase01/metodo_join.py
@@ -5,3 +5,19 @@ tupla_str = ('Hola', 'alumnos', 'Tecnicatura', 'Universitaria')
 
 mensaje = ' '.join(tupla_str)
 print(f'Mensaje: {mensaje}')
+
+lista_cursos = ['Java', 'Python', 'Angular', 'Spring']
+mensaje = ', '.join(lista_cursos)
+print(f'Mensaje: {mensaje}')
+
+cadena = 'HolaMundo'
+mensaje = '.'.join(cadena)
+print(f'Mensaje: {mensaje}')
+
+diccionario = {'nombre': 'Juan', 'apellido': 'Perez', 'edad': '18'}
+llaves = '-'.join(diccionario.keys())
+valores = '-'.join(diccionario.values())
+print(f'Llaves: {llaves}, Type: {type(llaves)}')
+print(f'valores: {valores}, Type: {type(llaves)}')
+
+
