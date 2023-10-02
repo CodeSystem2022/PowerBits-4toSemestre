81dfa5b388eb5856548c41b90cd2ef1242ff4419
Facundo Rios <112650077+jfacundorios@users.noreply.github.com>
2023-10-02 11:35:17 -0300

    Clase 6 Video 6.3

--- Node/01-Hola-Mundo/entorno.js
+++ Node/01-Hola-Mundo/entorno.js
@@ -1,5 +1,8 @@
 let nombre = process.env.NOMBRE || 'Sin nombre';
-let web = process.env.WEB  || 'No tengo web'
+let web = process.env.MI_WEB  || 'No tengo web'
 
 console.log('Hola ' + nombre)
 console.log('Mi web es: '+web)
+
+//LINK DE DOCUMENTACIÓN DE VARIABLES DE ENTORNO//
+//jairofernandez.medium.com/manejo-de-variables-de-entorno-en-node.js
