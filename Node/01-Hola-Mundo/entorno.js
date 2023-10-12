bd7bed26996a1a6d0e9f64a63758f7101c505ae2
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-10-12 16:32:38 -0300

    Clase 7- Video 7.1- Nodemon

--- Node/01-Hola-Mundo/entorno.js
+++ Node/01-Hola-Mundo/entorno.js
@@ -1,8 +1,10 @@
 let nombre = process.env.NOMBRE || 'Sin nombre';
-let web = process.env.MI_WEB  || 'No tengo web'
+let web = process.env.MI_WEB  || 'No tengo web';
 
-console.log('Hola ' + nombre)
-console.log('Mi web es: '+web)
+console.log('Hola ' + nombre);
+console.log('Mi web es: '+web);
+
+console.log('Ultima parte');
 
 //LINK DE DOCUMENTACIÓN DE VARIABLES DE ENTORNO//
 //jairofernandez.medium.com/manejo-de-variables-de-entorno-en-node.js
