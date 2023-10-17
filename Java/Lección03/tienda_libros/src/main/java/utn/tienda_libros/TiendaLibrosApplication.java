ddf4c4ed3532b53560f3fe2456b18d2446af59c6
Ivana <ayeivanagon@gmail.com>
2023-10-17 16:49:07 -0300

    Clase 8 - Video 8.2
    Terminando con el main - Parte 1

--- Java/Lección03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Lección03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,8 +1,13 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroFrom;
 
+import java.awt.EventQueue;
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
@@ -11,4 +16,14 @@ public class TiendaLibrosApplication {
 	}
 		ConfigurableApplicationContext contextoSpring =
 				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+
+		EventQueue.invokeLater(() ->{
+			//Obtenemos el objeto from a ttraves del spring
+		LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+		});
 }
+
+
