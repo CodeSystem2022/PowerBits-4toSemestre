52d8fdb15b47f217b1e976560a9f8b714708997b
juaniM4c <juanimachuca1@gmail.com>
2023-10-17 23:59:41 -0300

    Clase 8 - Video 8.3 (Parte 3)
    Avanzamos con swing y la tabla -> JTable (Parte 3)

--- Java/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -40,5 +40,24 @@ public class LibroFrom extends JFrame {
         this.tablaModeloLibros.setColumnIdentifiers(cabecera);
         //Instanciar el objeto de Jtable
         this.tablaLibros = new JTable(tablaModeloLibros);
+        listarLibros();
+    }
+
+    private void listarLibros(){
+        //Limpiar la tabla
+        tablaModeloLibros.setRowCount(0);
+        //Obtener los libros de la BD
+        var libros = libroServicio.listarLibros();
+        //Iteramos cada libro
+        libros.forEach((libro) -> { //Funcion Lamda
+            //Creamos cada registro para agregarlos a la tabla
+            Object [] renglonLibro = {
+                    libro.getIdLibro(),
+                    libro.getNombreLibro(),
+                    libro.getAutor(),
+                    libro.getPrecio(),
+                    libro.getExistencias(),
+            };
+        });
     }
 }
