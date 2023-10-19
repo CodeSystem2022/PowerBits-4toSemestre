139b8680d61d4e78a41bbc648cc2b8914a144b28
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-10-18 22:10:30 -0300

    Clase 8- Video 8.3- Generamos un callback

--- Node/02-Fundamentos_node/async/callback.js
+++ Node/02-Fundamentos_node/async/callback.js
@@ -1,14 +1,12 @@
 
-function soyAsincrona(){
-    setTimeout( function (miCallback) {
-        console.log('Hola, soy una funcion asincrona')
+function soyAsincrona(miCallback){
+    setTimeout( function () {
+        console.log('Hola, soy una funcion asincrona');
+        miCallback();
     }, 1000);
-    ;
 }
 
 console.log('Iniciando el preceso...');
 soyAsincrona(function(){
     console.log('Terminando el proceso...');
 });
-
-
