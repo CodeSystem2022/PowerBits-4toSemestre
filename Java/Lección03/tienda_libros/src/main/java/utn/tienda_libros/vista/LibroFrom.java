4b6018fdb0630ce419c7dc88aceb81915c6018ff
Lucas Mendez <mendezlucas777@gmail.com>
2023-10-20 18:49:09 -0300

    Clase 9 - 9.2 - Java
    Botones para modificar, eliminar y damos forma visual

--- Java/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -7,8 +7,6 @@ import utn.tienda_libros.servicio.LibroServicio;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 @Component
 public class LibroFrom extends JFrame {
@@ -17,9 +15,11 @@ public class LibroFrom extends JFrame {
     private JTable tablaLibros;
     private JTextField libroTexto;
     private JTextField autorTexto;
-    private JTextField precioTextoTextField;
-    private JTextField existenciasTextoTextField;
+    private JTextField precioTexto;
+    private JTextField existenciasTexto;
     private JButton agregarButton;
+    private JButton modificarButton;
+    private JButton eliminarButton;
     private DefaultTableModel tablaModeloLibros;
 
     @Autowired
