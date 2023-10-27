5f820ef95deb57c215d3af0779da5d7990b08216
Lucas Mendez <mendezlucas777@gmail.com>
2023-10-27 20:53:22 -0300

    corregimos errores

--- Java/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -2,11 +2,14 @@ package utn.tienda_libros.vista;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import utn.tienda_libros.modelo.Libro;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 @Component
 public class LibroFrom extends JFrame {
@@ -38,19 +41,30 @@ public class LibroFrom extends JFrame {
         });
     }
 
-    private void iniciarForma(){...}
+    private void iniciarForma(){
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(900, 700);
+        //Para obtener las dimensiones de las ventanas
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getHeight()/2);
+        setLocation(x, y);
+    }
 
     private void agregarLibro() {
         //Leer los valores del formulario
         if (libroTexto.getText().equals("")) {
             mostrarMensaje("Ingresa el nombre del libro");
-            libroTexto.requesFocusInWindow();
+            libroTexto.requestFocusInWindow();
             return;
         }
         var nombreLibro = libroTexto.getText();
         var autor = autorTexto.getText();
         var precio = Double.parseDouble(precioTexto.getText());
-        var existencias = Integer.parsInt(existenciasTexto.getText());
+        var existencias = Integer.parseInt(existenciasTexto.getText());
         //Creamos el objeto del libro
         var libro = new Libro(null, nombreLibro, autor, precio, existencias);
         //libro.setNombreLibro(nombreLibro);
@@ -75,19 +89,6 @@ public class LibroFrom extends JFrame {
         autorTexto.setText("");
         precioTexto.setText("");
         existenciasTexto.setText("");
-    }
-
-
-        setContentPane(panel);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        setSize(900, 700);
-        //Para obtener las dimensiones de las ventanas
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension tamanioPantalla = toolkit.getScreenSize();
-        int x = (tamanioPantalla.width - getWidth()/2);
-        int y = (tamanioPantalla.height - getHeight()/2);
-        setLocation(x, y);
     }
 
     private void mostrarMensaje(String mensaje){
