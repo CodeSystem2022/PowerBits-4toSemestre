de50d7b50610373493ec37ae5743ed1897ecec4a
NadiaGonzalez27 <nadiagonzalez206@gmail.com>
2023-11-04 23:05:28 -0300

    Clase 11- Video 11.4- Definimos los resultados con async/await

--- Node/02-Fundamentos_node/async/asyncAwait.js
+++ Node/02-Fundamentos_node/async/asyncAwait.js
@@ -1,3 +1,5 @@
+//La palabra async no es necesaria en las funciones, porque ya son asincronas
+//Igual proyectan una sincronia visual
 async function hola(nombre){
     return new Promise(function (resolve, reject){
          setTimeout( function () {
@@ -20,20 +22,67 @@ async function hablar(nombre){
 async function adios(nombre){
     return new Promise((resolve, reject) => {
         setTimeout( function () {
+            //validamos el error o aprobación
             console.log('Adios '+ nombre);
+            //if(err) reject('Hay un error);
             resolve();
         }, 1000); 
     });
 }
 
 //await hola('Ariel'); // esto es una mala sintaxis
-
+// await solo es valido dentro de una funcion asincrona
 async function main(){
     let nombre = await hola('Ariel');
     await hablar();
     await hablar();
     await hablar();
     await adios(nombre);
+    console.log('Termina el proceso...')
 }
 
-main();
+//console.log('Empezamos el proceso...')
+//main();
+//console.log('Esta va a ser la segunda instrucción')
+ 
+//Código en ingles
+//Es asincronico y retorna una promesa
+async function sayHello(name){
+    return new Promise( (resolve, reject) => { //usamos la sintaxis ES6
+        setTimeout(function () {
+            console.log("Hello " + name);
+            resolve(name);
+        }, 1000);
+    });
+}
+
+async function talk(name){
+    return new Promise( (resolve, reject) => { 
+        setTimeout(function () {
+            console.log("Bla bla bla bla");
+            resolve(name);
+        }, 1000);
+    });
+}
+
+async function sayBye(name){
+    return new Promise((resolve, reject) => {
+        setTimeout( function () {
+            console.log("Goodbye " + name);
+            resolve(name);
+        }, 1000); 
+    });
+}
+
+async function conversation(name){
+    console.log('Code in english')
+    console.log("Starting async process...");
+    await sayHello(name);
+    await talk();
+    await talk();
+    await talk();
+    await sayBye(nombre);
+    console.log("Process completed...");
+}
+
+conversation("Ariel");
